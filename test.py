f54bab476c17ecfc60f28b9f07fa1f6ec0399c8b
Aleksandra Keše <aleksandra.kese@student.fmf.uni-lj.si>
2019-04-24 15:09:52 +0200

    still trying to get a merge conflict

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 # Napiši program, ki izpiše vsa praštevila manjša od 200.
 
-def je_prastevilo(n):
+def je_pfadjaflkjrastevilo(n):
     for i in range(2, n):
         if n % i == 0:
-            return False
-    return True
 
 for x in range(2, 201):
     if je_prastevilo(x):
         print(x)
+
+
+        hgfslfusčj
+        uhagiuie
