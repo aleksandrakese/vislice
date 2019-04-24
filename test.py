de7bdcfc4e7a833829b52dc1013cd94435140dc5
aleksandrakese <49947082+aleksandrakese@users.noreply.github.com>
2019-04-24 15:08:23 +0200

    Trying to get a merge conflict

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 # Napiši program, ki izpiše vsa praštevila manjša od 200.
 
-def je_prastevilo(n):
+def je_prastehfgahlglhlghvilo(n):
     for i in range(2, n):
-        if n % i == 0:
+        if n % i == 0: gafuhnafi
             return False
     return True
 
 for x in range(2, 201):
     if je_prastevilo(x):
         print(x)
+        
+        hfgil
+        gfalhfaluh
